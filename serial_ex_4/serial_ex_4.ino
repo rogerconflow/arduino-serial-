0d1b6a857c0ac86c4ef32141e0d034580e6c3971
rogerconflow <rogerlladom@gmail.com>
2015-04-27 21:09:21 +0200

    hgff
    nvc

---
+++ serial_ex_4/serial_ex_4.ino
@@ -0,0 +1,12 @@
+void setup()                // run once, when the sketch starts
+{
+  Serial.begin(9600);       // set up Serial library at 9600 bps
+  Serial.println("Escull el numero de l'operacio que vols realitzar ?");
+  Serial.println("Comprovar numero de tarjeta de credit");
+  Serial.println("Comprovar numero de compte bancari");
+  Serial.println("Busca un digit perdut de tarjeta de credit");
+}
+
+void loop () 
+{
+}
