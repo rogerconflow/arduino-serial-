447447d57f956cf02ac1b23328a984b449bc6fd2
rogerconflow <rogerlladom@gmail.com>
2015-05-04 21:22:49 +0200

    jdty
    dxgnfns

--- serial_ex_2.5/serial_ex_2.5.ino
+++ serial_ex_2.5/serial_ex_2.5.ino
@@ -1,22 +1,23 @@
 int drive_gb = 100;
-long drive_mb;     // we changed the type from "int" to "long"
+long drive_mb;
 
-void setup()             // run once, when the sketch starts
+void setup()               //run once, when the sketch starts
 {
-  Serial.begin(9600);    // set up Serial library at 9600 bps
+  Serial.begin(9600);       //set up Serial library at 9600 bps
 
   Serial.print("Your HD is ");
   Serial.print(drive_gb);
   Serial.println(" GB large.");
 
-  drive_mb = 1024 * drive_gb;
+  drive_mb = drive_gb;
+  drive_mb = drive_mb * 1024;
 
   Serial.print("It can store ");
   Serial.print(drive_mb);
   Serial.println(" Megabytes!");
 }
 
-void loop()   // we need this to be here even though its empty
+void loop()      //we need this to be here even though its empty
 {
 }
 
---
+++ serial_ex_2.6/serial_ex_2.6.ino
@@ -0,0 +1,19 @@
+int test = 32767;
+
+void setup()              // run once, when the sketch starts
+{
+  Serial.begin(9600);     //set up Serial library at 9600 bps
+  Serial.print("Test value is: ");
+  Serial.println(test);
+
+  test = test + 1;
+ 
+  Serial.print("Now it is ");
+  Serial.println(test);
+}
+
+void loop()   // we need this to be here even though its empty
+{
+}
+
+
